b2efe6a28f5ae16761bd851c19b270b282bec8e5
Maadeash <maadeash005@gmail.com>
2026-08-04 23:06:57 +0530

    Clean up blank lines in rv32i_tb_pkg.sv
    Removed unnecessary blank lines in rv32i_tb_pkg.sv.

--- UVM_VIP/rv32i_tb_pkg.sv
+++ UVM_VIP/rv32i_tb_pkg.sv
@@ -1,52 +1,18 @@
-
-
-
-
-
-
-
 `ifndef RV32I_TB_PKG_SV
 `define RV32I_TB_PKG_SV
-
 `include "uvm_macros.svh"
-
 package rv32i_tb_pkg;
     import uvm_pkg::*;
     import rv32i_pkg::*;
-
-
-
     `include "rv32i_seq_item.sv"
-
-
-
-
     `include "rv32i_monitor.sv"
-
-
-
-
     `include "rv32i_sequencer.sv"
     `include "rv32i_driver.sv"
     `include "rv32i_scoreboard.sv"
     `include "rv32i_coverage.sv"
-
-
-
-
     `include "rv32i_agent.sv"
     `include "rv32i_env.sv"
-
-
-
-
     `include "rv32i_sequence_lib.sv"
-
-
-
-
     `include "rv32i_test_lib.sv"
-
 endpackage : rv32i_tb_pkg
-
 `endif
