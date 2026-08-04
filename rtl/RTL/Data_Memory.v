8c2f4e8e4e848407330c6e51e645efd52f2603fb
Maadeash <maadeash005@gmail.com>
2026-08-04 20:39:23 +0530

    Add files via upload

---
+++ rtl/RTL/Data_Memory.v
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+
+
+module Data_Memory #(
+    parameter INIT_FILE = ""
+)(
+    input         clk,
+    input         rst,
+    input         WE,
+    input  [3:0]  WSTRB,
+    input  [31:0] A,
+    input  [31:0] WD,
+    output reg [31:0] RD
+);
+    localparam MEM_WORDS = 256;
+
+    reg [31:0] ram [0:MEM_WORDS-1];
+
+    integer i;
+    initial begin
+        for (i = 0; i < MEM_WORDS; i = i + 1)
+            ram[i] = 32'd0;
+        if (INIT_FILE != "")
+            $readmemh(INIT_FILE, ram);
+    end
+
+    always @(posedge clk or negedge rst) begin
+        if (!rst) begin
+            RD <= 32'd0;
+        end else begin
+            RD <= ram[A[9:2]];
+            if (WE) begin
+                if (WSTRB[0]) ram[A[9:2]][7:0]   <= WD[7:0];
+                if (WSTRB[1]) ram[A[9:2]][15:8]  <= WD[15:8];
+                if (WSTRB[2]) ram[A[9:2]][23:16] <= WD[23:16];
+                if (WSTRB[3]) ram[A[9:2]][31:24] <= WD[31:24];
+            end
+        end
+    end
+endmodule
---
+++ rtl/RTL/Sign_Extend.v
@@ -0,0 +1,18 @@
+
+`include "rv32i_defs.v"
+module Sign_Extend (
+    input  [31:0] In,
+    input  [2:0]  ImmSrc,
+    output reg [31:0] Imm_Ext
+);
+    always @* begin
+        case (ImmSrc)
+            3'b000: Imm_Ext = {{20{In[31]}}, In[31:20]};
+            3'b001: Imm_Ext = {{20{In[31]}}, In[31:25], In[11:7]};
+            3'b010: Imm_Ext = {{19{In[31]}}, In[31], In[7], In[30:25], In[11:8], 1'b0};
+            3'b011: Imm_Ext = {In[31:12], 12'd0};
+            3'b100: Imm_Ext = {{11{In[31]}}, In[31], In[19:12], In[20], In[30:21], 1'b0};
+            default: Imm_Ext = 32'd0;
+        endcase
+    end
+endmodule
