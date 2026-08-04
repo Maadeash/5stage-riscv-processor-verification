e62272745aaab37917665c679f064dfa7429a9f7
Maadeash <maadeash005@gmail.com>
2026-08-04 23:05:57 +0530

    Clean up rv32i_sequencer.sv by removing blank lines
    Removed unnecessary blank lines from rv32i_sequencer.sv.

--- UVM_VIP/rv32i_sequencer.sv
+++ UVM_VIP/rv32i_sequencer.sv
@@ -1,21 +1,13 @@
-
-
-
-
 `ifndef RV32I_SEQUENCER_SV
 `define RV32I_SEQUENCER_SV
-
 `include "uvm_macros.svh"
 import uvm_pkg::*;
 import rv32i_pkg::*;
 
 class rv32i_sequencer extends uvm_sequencer #(rv32i_seq_item);
     `uvm_component_utils(rv32i_sequencer)
-
     function new(string name = "rv32i_sequencer", uvm_component parent = null);
         super.new(name, parent);
     endfunction
-
 endclass : rv32i_sequencer
-
 `endif
