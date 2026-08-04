c7eee98d1817f1e6c151d454b226226a9e1dedd5
Maadeash <maadeash005@gmail.com>
2026-08-04 22:26:24 +0530

    Update rv32i_agent.sv

--- UVM_VIP/rv32i_agent.sv
+++ UVM_VIP/rv32i_agent.sv
@@ -1,25 +1,14 @@
-
-
-
-
-
-
-
 `ifndef RV32I_AGENT_SV
 `define RV32I_AGENT_SV
-
 `include "uvm_macros.svh"
 import uvm_pkg::*;
 import rv32i_pkg::*;
 
 class rv32i_agent extends uvm_agent;
     `uvm_component_utils(rv32i_agent)
-
     rv32i_sequencer  sequencer;
     rv32i_driver     driver;
     rv32i_monitor    monitor;
-
-
     uvm_analysis_port #(rv32i_obs_item) ap_wb;
     uvm_analysis_port #(rv32i_obs_item) ap_mem;
     uvm_analysis_port #(rv32i_obs_item) ap_trap;
@@ -34,7 +23,6 @@ class rv32i_agent extends uvm_agent;
         sequencer = rv32i_sequencer::type_id::create("sequencer", this);
         driver    = rv32i_driver   ::type_id::create("driver",    this);
         monitor   = rv32i_monitor  ::type_id::create("monitor",   this);
-
         ap_wb   = new("ap_wb",   this);
         ap_mem  = new("ap_mem",  this);
         ap_trap = new("ap_trap", this);
@@ -42,16 +30,11 @@ class rv32i_agent extends uvm_agent;
     endfunction
 
     function void connect_phase(uvm_phase phase);
-
         driver.seq_item_port.connect(sequencer.seq_item_export);
-
-
         monitor.ap_wb.connect(ap_wb);
         monitor.ap_mem.connect(ap_mem);
         monitor.ap_trap.connect(ap_trap);
         monitor.ap_all.connect(ap_all);
     endfunction
-
 endclass : rv32i_agent
-
 `endif
